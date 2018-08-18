b4dc31ce1a5fa7b3cc2d1cd78e66292fcb0f4fa3
seungsol <39159043+seungsol@users.noreply.github.com>
2018-08-18 18:09:53 +0900

    Add files via upload

---
+++ testing.sol
@@ -0,0 +1,14 @@
+﻿pragma solidity ^0.4.0; 
+ 
+contract SimpleStorage {
+    uint storedData;
+ 
+    function set(uint x) {
+    	x=x+1;
+        storedData = x;
+    }
+ 
+    function get() constant returns (uint) {
+        return storedData;
+    }
+}
